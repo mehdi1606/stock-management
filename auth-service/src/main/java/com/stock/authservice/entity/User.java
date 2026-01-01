dbc831372da50dec4f6124d18d7acc98c7f81c76
Semlali20 <semlalilahcen73@gmail.com>
2026-01-01 13:20:35 +0100

    merboha

--- auth-service/src/main/java/com/stock/authservice/entity/User.java
+++ auth-service/src/main/java/com/stock/authservice/entity/User.java
@@ -88,7 +88,7 @@ public class User {
     @Column(name = "locked_until")
     private LocalDateTime lockedUntil;
 
-    @Column(name = "profile_image_url", columnDefinition = "LONGTEXT")
+    @Column(name = "profile_image_url", columnDefinition = "TEXT")
     private String profileImageUrl;
 
     @Column(name = "language", length = 10)
--- quality-service/src/main/java/com/stock/qualityservice/entity/Disposition.java
+++ quality-service/src/main/java/com/stock/qualityservice/entity/Disposition.java
@@ -7,5 +7,6 @@ public enum Disposition {
     SCRAP,               // Dispose/scrap
     CONDITIONAL_ACCEPT,  // Accept with conditions
     RETURN_TO_SUPPLIER,  // Return to vendor
-    UNDER_REVIEW         // Pending decision
+    UNDER_REVIEW ,        // Pending decision
+    QUARANTINE
 }
